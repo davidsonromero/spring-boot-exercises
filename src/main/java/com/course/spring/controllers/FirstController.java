bb8be2d94ac3cd5b760e3679e4b6049151cade17
davidsonromero <davikromero@hotmail.com>
2024-01-24 00:28:16 -0300

    First web service

---
+++ src/main/java/com/course/spring/controllers/FirstController.java
@@ -0,0 +1,13 @@
+package com.course.spring.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    @GetMapping(path = {"/hello", "/"})
+    public String hello() {
+        return "Vai Corinthians!";
+    }
+}
